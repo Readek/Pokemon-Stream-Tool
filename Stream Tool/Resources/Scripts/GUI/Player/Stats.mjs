889a79129227b5158d23ecbd38843eb36858505e
Readek <readeku@gmail.com>
2024-03-26 17:20:24 +0100

    Increase stat on icon click

--- Stream Tool/Resources/Scripts/GUI/Player/Stats.mjs
+++ Stream Tool/Resources/Scripts/GUI/Player/Stats.mjs
@@ -1,20 +1,30 @@
+const catchesInput = document.getElementById("catchesInput");
+const catchesIcon = document.getElementById("statsCatchesIcon");
+const deathsInput = document.getElementById("deathsInput");
+const deathsIcon = document.getElementById("statsDeathsIcon");
+
 class PlayerStats {
 
-    #catchesInput = document.getElementById("catchesInput");
-    #deathsInput = document.getElementById("deathsInput");
+    constructor() {
+
+        // when clicking on stats icon, increase stat by 1, for convenience
+        catchesIcon.addEventListener("click", () => {this.setCatches(this.getCatches()+1)});
+        deathsIcon.addEventListener("click", () => {this.setDeaths(this.getDeaths()+1)});
+
+    }
 
     getCatches() {
-        return Number(this.#catchesInput.value);
+        return Number(catchesInput.value);
     }
     setCatches(value) {
-        this.#catchesInput.value = value;
+        catchesInput.value = value;
     }
 
     getDeaths() {
-        return Number(this.#deathsInput.value);
+        return Number(deathsInput.value);
     }
     setDeaths(value) {
-        this.#deathsInput.value = value;
+        deathsInput.value = value;
     }
 
 }
