e68231a65f254db645fc5acb436bed5ff3b3b87f
Readek <readeku@gmail.com>
2024-04-22 12:36:10 +0200

    Fix party reorder not updating overlay

--- Stream Tool/Resources/Scripts/GUI/Emu Scripts/Citra/Raw Pokes/Raw Pokes.mjs
+++ Stream Tool/Resources/Scripts/GUI/Emu Scripts/Citra/Raw Pokes/Raw Pokes.mjs
@@ -1,3 +1,4 @@
+import { current } from "../../../Globals.mjs";
 import { RawPokemonBattle } from "./Raw Pokemon Battle.mjs";
 import { RawPokemonParty } from "./Raw Pokemon Party.mjs";
 
@@ -19,6 +20,9 @@ export const rawBattlePokes = [
     new RawPokemonBattle
 ]
 
+// to check if order changed
+let prevIndexes = [0, 0, 0, 0, 0, 0]
+
 /**
  * Reorders party to ingame order. 
  * Game's party memory rarely shuffles order, instead the game has an
@@ -27,9 +31,26 @@ export const rawBattlePokes = [
  * @returns {RawPokemonParty[]} Ordered party as seen in-game
  */
 export function indexRawParty(indexes) {
+
+    // check if order changed
+    for (let i = 0; i < indexes.length; i++) {
+        if (indexes[i] != prevIndexes[i]) {
+            current.autoUpdated = true;
+            prevIndexes = indexes;
+            break;
+        }
+    }
+
+    // reorder that party
     const rawPokesIndexed = [];
     for (let i = 0; i < rawPartyPokes.length; i++) {
+
         rawPokesIndexed.push(rawPartyPokes[indexes[i]]);
+        if (current.autoUpdated) {
+            rawPokesIndexed[i].changeHasChanged();
+        }
+
     }
+
     return rawPokesIndexed;
 }
